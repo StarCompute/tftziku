1eccd38b71e241c8f4b344b018c9ae8762cb9a45
StarCompute <506686@qq.com>
2024-03-29 21:54:58 +0800

    补充注释

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@ XFont *_xFont;
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Arduino_GFX Hello World example");
-   _xFont  = new XFont();
+  Serial.println("Init.");
+  // 注意，xFont的初始化必须在setup内
+  _xFont = new XFont();
   _xFont->DrawChinese(10, 10, "业精于勤荒于嬉戏，行成于思毁于随。", GREEN);
-  
 }
 
 void loop()
